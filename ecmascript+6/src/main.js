f32bf2fca6a69c7842724343b18a6023e83ea976
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2023-05-22 09:19:14 -0300

    finalizar e arrumar erros

--- ecmascript+6/src/main.js
+++ ecmascript+6/src/main.js
@@ -1,30 +1,49 @@
-import { createApp} from "vue"
+import { createApp } from "vue";
 import Todos from "./api/todos";
-import "./assets/css/main.css"
+import "./assets/css/main.css";
 
-const apiTodos = new Todos()
+const apiTodos = new Todos();
 
-async function exec(){
-  const todos = await apiTodos.inde()
+async function exec() {
+  const todos = await apiTodos.inde();
 
-  const ul = document.querySelector("ul.todos")
+  const ul = document.querySelector("ul.todos");
 
-  todos.forEach((todo) =>{
+  todos.forEach((todo) => {
+    // Use a variável "ul" aqui para manipular a lista
+    ul.innerHTML += `<li>${todo}</li>`;
+  });
 
-  })
-}
-exec()
+  const app = createApp({
+    data() {
+      return {
+        todos: [],
+        form:{
+          text:"",
+          done:false
+        }
+      };
+    },
+    created() {
+      this.fetchTodos();
+    },
+    methods: {
+      async fetchTodos() {
+        this.todos = await apiTodos.inde();
+        console.log(this.todos);
+      },
+      async createTodo(){
+        const data = await apiTodos.store(this.form)
+        this.todos.push(data)
 
-const app = createApp({
-    data(){
-      return{
-         todos:[]
+        this.form.text = ""
+        this.form.done = false
       }
     },
-    created(){
-      console.log()
-    },
-   
-})
+  });
 
-app.mount("#app")
+  app.mount("#app");
+}
+
+exec();
+
